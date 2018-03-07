7255575aa76613e057614fd0fc36cf800e763288
Ladence <zakhar.karlin11@gmail.com>
2018-03-08 01:00:39 +0300

    implemented simple blockchain structure

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	bc := NewBlockchain()
+
+	bc.AddNewBlock("First transaction")
+	bc.AddNewBlock("Second transaction")
+	bc.AddNewBlock("Third transaction")
+
+	for _, block := range bc.blocks {
+		fmt.Printf("Data %s\n", block.Data)
+		fmt.Printf("Prev hash %x\n", block.PrevBlockHash)
+		fmt.Printf("Current hash %x\n", block.Hash)
+
+		fmt.Println()
+	}
+}
